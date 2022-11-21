c02a9e7d0259fdc33d62dc8edf85f5bec54e54c6
infosecual <davidjtheo@gmail.com>
2022-11-21 14:34:00 -0600

    docker reinstatiates each func while fuzzing

--- nosy_neighbor.go
+++ nosy_neighbor.go
@@ -123,15 +123,19 @@ func generate_fuzz_scripts(target_dir string, docker_repo_path string) []string 
 	seconds := TargetConfig.TestTimeSeconds
 	for i := 0; i < len(FuzzFunctions)/2; i++ {
 		script := ""
+		script += fmt.Sprintf("echo \"fixing up GOROOT for fuzzing\"\n")
+		script += fmt.Sprintf("rm -rf go/*\n")
+		script += fmt.Sprintf("cp -rp /go_backup/* /go\n")
+		script += fmt.Sprintf("cd %s\n", docker_repo_path)
 		script += fmt.Sprintf("echo \"Fuzzing function %s for %d seconds\"\n", FuzzFunctions[2*i], seconds)
 		script += fmt.Sprintf("cd %s\n", FuzzFunctions[2*i+1])
-		script += fmt.Sprintf("%s test -fuzz=%s -fuzztime=%ds\n", TargetConfig.TargetGoVersion, FuzzFunctions[2*i], seconds)
+		script += fmt.Sprintf("%s test -fuzz=%s -fuzztime=%ds -test.fuzzcachedir=/cache/\n", TargetConfig.TargetGoVersion, FuzzFunctions[2*i], seconds)
 		script += fmt.Sprintf("if [ -d \"./testdata/fuzz\" ]; then\n")
-		script += fmt.Sprintf("\tmv ./testdata/fuzz/* %s/nosy_fuzz_dir/\n", docker_repo_path)
+		script += fmt.Sprintf("\tmv ./testdata/fuzz/* /results/\n")
 		script += fmt.Sprintf("\trm -rf ./testdata/fuzz/*\n")
 		script += fmt.Sprintf("\techo \"cd %s && go test -run=%s/nosy_fuzz_dir/%s/.\"\n", FuzzFunctions[2*i+1], docker_repo_path, FuzzFunctions[2*i])
 		script += fmt.Sprintf("fi\n")
-		filename := fmt.Sprintf("%s/nosy_fuzz_dir/fuzz_%s.sh", target_dir, FuzzFunctions[2*i])
+		filename := fmt.Sprintf("%s/fuzz_%s.sh", target_dir, FuzzFunctions[2*i])
 		docker_relative_filename := fmt.Sprintf("fuzz_%s.sh", FuzzFunctions[2*i])
 		scripts = append(scripts, docker_relative_filename)
 		f, err := os.Create(filename)
@@ -278,8 +282,28 @@ func fuzz() {
 		TargetConfig.TargetRepo,
 		TargetConfig.TargetRepoImportPrefix)
 
+	// get the path that we should place the fuzzers shell script in
+	// this must be the absolute path for docker dir binding
+	asset_dir := fmt.Sprintf("%s/fuzzing_directory/%s/scripts",
+		pwd,
+		TargetConfig.TargetRepo)
+
+	// make a directory to hold the fuzzing scripts
+	cmd := fmt.Sprintf("mkdir %s", asset_dir)
+	exec_and_print(cmd)
+
 	// this is the to $GOROOT in the docker container
 	local_goroot_path := fmt.Sprintf("%s/fuzzing_directory/%s/go",
+		pwd,
+		TargetConfig.TargetRepo)
+
+	// this is the path to the cache dir where new test cases are saved
+	local_cache_path := fmt.Sprintf("%s/fuzzing_directory/%s/cache",
+		pwd,
+		TargetConfig.TargetRepo)
+
+	// this is the path to the results dir where new test cases are saved
+	local_results_path := fmt.Sprintf("%s/fuzzing_directory/%s/results",
 		pwd,
 		TargetConfig.TargetRepo)
 
@@ -303,13 +327,15 @@ func fuzz() {
 		log.Fatal(err)
 	}
 
-	scripts := generate_fuzz_scripts(local_repo_path, docker_repo_path)
+	scripts := generate_fuzz_scripts(asset_dir, docker_repo_path)
 
 	fmt.Println("Fuzzing", len(FuzzFunctions)/2, "functions...")
 
+	// Iterate through target functions, fix the GOROOT, fuzz the function,
+	// save the offending test cases
 	for _, script := range scripts {
-		command := fmt.Sprintf("docker run --workdir %s/nosy_fuzz_dir -v %s:/go nosy-neighbor %s\n",
-			docker_repo_path, local_goroot_path, script)
+		command := fmt.Sprintf("docker run --workdir /scripts -v %s:/scripts -v %s:/go_backup -v %s:/cache -v %s:/results nosy-neighbor %s\n",
+			asset_dir, local_goroot_path, local_cache_path, local_results_path, script)
 		exec_and_print(command)
 	}
 
